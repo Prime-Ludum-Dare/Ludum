eb70b9369c8c6350778e1f7e1500144990031e5c
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-12-01 13:58:10 -0600

    player moved into separate js

--- scripts/client.js
+++ scripts/client.js
@@ -13,7 +13,10 @@ let playerY = 150;
 let rightPressed = false;
 let leftPressed = false;
 let upPressed = false;
+
 let downPressed = false;
+
+const worldList = [];
 
 const init = () => {
   // get the canvas so we can draw on it
@@ -54,23 +57,26 @@ const drawPlayer = () => {
 };
 
 const draw = () => {
-  // clear the screen
+  // // clear the screen
   ctx.fillStyle = 'rgba(200, 200, 200, 0.5)';
   ctx.fillRect(0, 0, 800, 600);
 
-  // handle movement
-  if (rightPressed && playerX < canvas.width - playerWidth) {
-    playerX += playerSpeed;
-  } else if (leftPressed && playerX > 0) {
-    playerX -= playerSpeed;
-  }
+  // // handle movement
+  // if (rightPressed && playerX < canvas.width - playerWidth) {
+  //   playerX += playerSpeed;
+  // } else if (leftPressed && playerX > 0) {
+  //   playerX -= playerSpeed;
+  // }
 
-  if (downPressed && playerY < canvas.height - playerHeight) {
-    playerY += playerSpeed;
-  } else if (upPressed && playerY > 0) {
-    playerY -= playerSpeed;
-  }
+  // if (downPressed && playerY < canvas.height - playerHeight) {
+  //   playerY += playerSpeed;
+  // } else if (upPressed && playerY > 0) {
+  //   playerY -= playerSpeed;
+  // }
 
-  // draw the things
-  drawPlayer();
+  // // draw the things
+  // drawPlayer();
+  for(object of worldList){
+    object.render();
+  }
 };
