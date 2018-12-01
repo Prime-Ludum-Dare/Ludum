8711080e40f20c778a4c8e524609ac94aa62e47e
Joseph Carey <josephcareycomposer@gmail.com>
2018-12-01 12:21:51 -0600

    center, background

--- scripts/client.js
+++ scripts/client.js
@@ -1,11 +1,17 @@
 console.log('client.js loaded');
 
 const init = () => {
+  // get the canvas so we can draw on it
   var canvas = document.getElementById('mainCanvas');
   var ctx = canvas.getContext('2d');
+
+  // some example rectangles
   ctx.fillStyle = 'rgb(200, 0, 0)';
   ctx.fillRect(10, 10, 50, 50);
 
   ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
   ctx.fillRect(30, 30, 50, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 100, 0.5)';
+  ctx.fillRect(0, 0, 800, 600);
 };
