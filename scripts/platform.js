e39dac753be08339ba1702f7c07bdaa257899a0f
Joseph Carey <josephcareycomposer@gmail.com>
2018-12-02 17:05:49 -0600

    basic dying and spawning corpses

--- scripts/platform.js
+++ scripts/platform.js
@@ -1,14 +1,23 @@
 class Platform {
-  constructor(X, Y, width) {
+  constructor(X, Y, width, hasBoar = false) {
     this.X = X;
     this.Y = Y;
     this.width = width;
-    platformList.push(this);
+    let thisPlatform = platformList.push(this);
     sortPlatforms();
+    if (hasBoar) {
+      new Boar(thisPlatform - 1);
+    }
   }
 
   render() {
     ctx.fillStyle = 'rgba(100,255,100,1)';
     ctx.fillRect(this.X - camera.X, this.Y - camera.Y, this.width, 5);
+  }
+}
+
+class PlatformWithBoar extends Platform {
+  constructor(X, Y, width) {
+    super(X, Y, width, true);
   }
 }
